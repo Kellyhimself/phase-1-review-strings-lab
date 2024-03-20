72fb382be9b8ca72dce07b8276f43f7b7e111b3f
Kitui Kelly <kituikelly@gmail.com>
2024-03-20 22:14:32 +0300

    completes the lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+// Write your code in this file!
+const currentUser = 'Grace Hopper';
+let welcomeMessage="Welcome to Flatbook, "
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, '
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+let shortGreeting= "Welcome, "
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+shortGreeting=`Welcome, ${firstInitial}!`
